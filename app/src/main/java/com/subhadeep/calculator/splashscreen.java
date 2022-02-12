24648379dff2b7cce19b3a626c60325a54de995f
SubhadeepZilong <63146468+SubhadeepZilong@users.noreply.github.com>
2022-02-12 15:33:10 +0530

    code

--- app/src/main/java/com/subhadeep/calculator/splashscreen.java
+++ app/src/main/java/com/subhadeep/calculator/splashscreen.java
@@ -22,6 +22,6 @@ public class splashscreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },2000);
+        },1500);
     }
 }
